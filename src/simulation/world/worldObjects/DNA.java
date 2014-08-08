889e6a1b9961b5ec569d1b8789b83a0eeaca760f
Jarato <paulf@iesy.net>
2014-08-08 14:43:29 +0200

    Issue #3: filled class
    Issue #11: added needed methods for class: NeuralCell
    
    Added generic class Pair<V,K>

--- src/simulation/world/worldObjects/DNA.java
+++ src/simulation/world/worldObjects/DNA.java
@@ -1,5 +1,69 @@
 package simulation.world.worldObjects;
 
+/**
+ * @author Jarato (Paul David Fährmann)
+ * 
+ */
+
 public class DNA
 {
+	private double[]	genes;
+	private double		maxValue;
+	private double		minValue;
+
+	public int getNumberOfGenes()
+	{
+		if (genes != null)
+		{
+			return genes.length;
+		}
+		return 0;
+	}
+
+	public double[] getGenes()
+	{
+		return genes;
+	}
+
+	public void setGenes(double[] genes)
+	{
+		this.genes = genes;
+	}
+
+	public double getMaxValue()
+	{
+		return maxValue;
+	}
+
+	public void setMaxValue(double maxValue)
+	{
+		this.maxValue = maxValue;
+	}
+
+	public double getMinValue()
+	{
+		return minValue;
+	}
+
+	public void setMinValue(double minValue)
+	{
+		this.minValue = minValue;
+	}
+
+	public double[] normTo(double pMin, double pMax)
+	{
+		if (pMin > pMax)
+		{
+			throw new IllegalArgumentException("min-value must be less than max-value!");
+		}
+		else
+		{
+			double[] ret = new double[genes.length];
+			for (int i = 0; i < genes.length; i++)
+			{
+				ret[i] = (genes[i] - minValue) / (maxValue - minValue) * (pMax - pMin) + pMin;
+			}
+			return ret;
+		}
+	}
 }
--- src/simulation/world/worldObjects/neural/NeuralCell.java
+++ src/simulation/world/worldObjects/neural/NeuralCell.java
@@ -1,6 +1,82 @@
 package simulation.world.worldObjects.neural;
 
+/**
+ * @author Jarato (Paul David Fährmann)
+ */
+
+import simulation.world.worldObjects.DNA;
+import util.Pair;
+
 public class NeuralCell
 {
-	private double	memory	= 0.0;
+	private Pair<NeuralCell, Double>[]	link	= null;
+	private double						memory	= 0.0;
+
+	public NeuralCell(NeuralCell... pConnectedCells)
+	{
+		if (pConnectedCells != null)
+		{
+			this.link = new Pair[pConnectedCells.length];
+			for (int i = 0; i < pConnectedCells.length; i++)
+			{
+				link[i].setX(pConnectedCells[i]);
+			}
+		}
+	}
+
+	public double getMemory()
+	{
+		return memory;
+	}
+
+	public void setMemory(double pMemory)
+	{
+		this.memory = pMemory;
+	}
+
+	public void addMemory(double pMemory)
+	{
+		this.memory += pMemory;
+	}
+
+	public int getNumberOfNeededGenes()
+	{
+		if (link != null)
+		{
+			return link.length;
+		}
+		return 0;
+	}
+
+	/**
+	 * Pushes the stored value (= memory) to the connected cells.
+	 * Thereafter, the stored value is reset to 0.
+	 */
+	public void push()
+	{
+		if (this.memory != 0.0)
+		{
+			for (int i = 0; i < link.length; i++)
+			{
+				link[i].getX().addMemory(memory * link[i].getY());
+			}
+			memory = 0;
+		}
+	}
+
+	public void construct(DNA pDNA)
+	{
+		if (pDNA.getNumberOfGenes() != getNumberOfNeededGenes())
+		{
+			throw new IllegalArgumentException("length mismatch: number of linkvalues (" + pDNA.getNumberOfGenes() + ") has to match number of links (" + getNumberOfNeededGenes() + ")!");
+		}
+		else
+		{
+			double[] normed = pDNA.normTo(-1, +1);
+			for (int i = 0; i < link.length; i++)
+			{
+				link[i].setY(normed[i]);
+			}
+		}
+	}
 }
